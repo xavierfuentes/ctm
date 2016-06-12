b55c14458d75702a637edb56beaf02dad67324d1
Xavi <xavifuefer@gmail.com>
2016-06-12 18:14:04 +0100

    little refactor

--- src/line.js
+++ src/line.js
@@ -1,6 +1,31 @@
 class Line {
   constructor(lineString = '00|00|00|00|00|00|00|00|00|00||') {
     this.lineString = lineString
+    this.frames = this.getFrames(lineString)
+  }
+
+  /**
+   * Calculates the scoring for an entire line
+   * @return {Integer}   Line score
+   */
+  getScore() {
+    return this.frames // gets all the frames
+      .map(this.getFrameScore) // gets frame scoring
+      .reduce((prev, curr) => prev + curr) // sums all frames
+  }
+
+  /**
+   * Gets the normal (not bonus) frames from a string
+   * @return {Array}   Frames that form the bowling line
+   */
+  getFrames(lineString) {
+    const frames = lineString.split('||')[0].split('|') // split normal frames from the bonus ones
+
+    // 10 is for the 2 bonus rounds
+    if (frames.length > 10) throw Error('The max number of frames is 10')
+
+    // split throws (turns 2 characters into an array)
+    return frames.map((frame) => frame.split(''))
   }
 
   /**
@@ -9,22 +34,11 @@ class Line {
    * @return {Integer}           Frame score
    */
   getFrameScore(frame) {
-    return frame.split('') // split throws (turns 2 characters into an array)
-    .reduce((prev, curr) => {
-      // turns each character into a number and sums them
-      return Number(prev) + Number(curr)
-    }, 0) // intial score of 0
-  }
-
-  /**
-   * Calculates the scoring for an entire line
-   * @return {Integer}   Line score
-   */
-  getScore() {
-    return this.lineString
-      .split('|') // split frames
-      .map(this.getFrameScore) // gets frame scoring
-      .reduce((prev, curr) => prev + curr) // sums all frames
+    return frame
+      .reduce((prev, curr) => {
+        // turns each character into a number and sums them
+        return Number(prev) + Number(curr)
+      }, 0) // intial score of 0
   }
 }
 
